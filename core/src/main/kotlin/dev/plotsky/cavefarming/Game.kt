85385e521d73b49f1a7633a78f609005199f1942
Derrick Plotsky <derrickplotsky@gmail.com>
2021-04-03 15:00:11 -0700

    Update our existing screens and Game
    The Game now builds the new screen and binds a bunch more to our inject
    system.
    
    And now the LoadingScreen will setup the OverWorldScreen and remove
    itself when necessary.

--- core/src/main/kotlin/dev/plotsky/cavefarming/Game.kt
+++ core/src/main/kotlin/dev/plotsky/cavefarming/Game.kt
@@ -1,9 +1,12 @@
 package dev.plotsky.cavefarming
 
+import com.badlogic.ashley.core.PooledEngine
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import dev.plotsky.cavefarming.screens.LoadingScreen
+import dev.plotsky.cavefarming.screens.OverWorldScreen
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.inject.Context
@@ -15,12 +18,27 @@ class Game : KtxGame<KtxScreen>() {
     override fun create() {
         context.register {
             bindSingleton<Batch>(SpriteBatch())
-            bindSingleton(BitmapFont())
-            addScreen(LoadingScreen(this@Game, inject(), inject()))
+            val font = BitmapFont().apply {
+                data.markupEnabled = true
+            }
+            bindSingleton(font)
+            // The camera ensures we can render using our target resolution of 800x480
+            //    pixels no matter what the screen resolution is.
+            bindSingleton(OrthographicCamera().apply {
+                setToOrtho(false, 800f, 480f)
+            })
+            bindSingleton(PooledEngine())
+            addScreen(LoadingScreen(this@Game, inject(), inject(), inject()))
+            addScreen(OverWorldScreen(inject(), inject(), inject(), inject()))
         }
 
         setScreen<LoadingScreen>()
 
         super.create()
+    }
+
+    override fun dispose() {
+        context.dispose()
+        super.dispose()
     }
 }
--- core/src/main/kotlin/dev/plotsky/cavefarming/screens/LoadingScreen.kt
+++ core/src/main/kotlin/dev/plotsky/cavefarming/screens/LoadingScreen.kt
@@ -1,5 +1,8 @@
 package dev.plotsky.cavefarming.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import dev.plotsky.cavefarming.Game
@@ -9,12 +12,20 @@ import ktx.graphics.use
 class LoadingScreen(
     private val game: Game,
     private val batch: Batch,
-    private val font: BitmapFont
+    private val font: BitmapFont,
+    private val camera: OrthographicCamera
 ) : KtxScreen {
     override fun render(delta: Float) {
-        batch.use {
-            font.draw(it, "Welcome to Cave Farming!", 200f, 400f)
+        batch.use(camera) {
+            font.draw(it, "[GOLDENROD]Welcome to Cave Farming!", 200f, 400f)
         }
+
+        if (Gdx.input.isTouched || Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            game.setScreen<OverWorldScreen>()
+            game.removeScreen<LoadingScreen>()
+            dispose()
+        }
+
         super.render(delta)
     }
 }
