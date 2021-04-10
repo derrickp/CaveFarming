c5963640b7796196389c77c65c8936d929b1d5b2
Derrick Plotsky <derrickplotsky@gmail.com>
2021-04-10 01:39:19 -0700

    Add new assets
    Now I can have a background and eventually some seeds for the crops.

---
+++ assets/floor.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="floor" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="pieces/floor_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="pieces/floor_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="pieces/floor_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="pieces/floor_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="pieces/floor_5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="pieces/floor_6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="pieces/floor_7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="pieces/floor_8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="pieces/hole.png"/>
+ </tile>
+</tileset>
