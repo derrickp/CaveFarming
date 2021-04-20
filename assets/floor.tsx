efe764466c4dc94bb1ee2a7bcdf07d86119ad65c
Derrick Plotsky <derrickplotsky@gmail.com>
2021-04-19 22:55:26 -0700

    Update some assets
    None of these are great (obviously), but I'm updating them for some new
    tries by me.

--- assets/floor.tsx
+++ assets/floor.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="floor" tilewidth="16" tileheight="16" tilecount="9" columns="0">
+<tileset version="1.5" tiledversion="1.5.0" name="floor" tilewidth="24" tileheight="24" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="16" source="pieces/floor_1.png"/>
@@ -27,5 +27,8 @@
  </tile>
  <tile id="8">
   <image width="16" height="16" source="pieces/hole.png"/>
+ </tile>
+ <tile id="9">
+  <image width="24" height="24" source="../../test_assets/background_1.png"/>
  </tile>
 </tileset>
