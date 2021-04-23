e820cecd6602d654264e17c0cf5057a4241c4557
Derrick Plotsky <derrickplotsky@gmail.com>
2021-04-22 19:01:17 -0700

    Add some box2d variables to MoveComponent
    We need more than just the single speed now for our interactions with
    Box2D and its world.

--- core/src/main/kotlin/dev/plotsky/cavefarming/components/MoveComponent.kt
+++ core/src/main/kotlin/dev/plotsky/cavefarming/components/MoveComponent.kt
@@ -1,10 +1,19 @@
 package dev.plotsky.cavefarming.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.math.Vector2
 import ktx.ashley.mapperFor
 
 class MoveComponent : Component {
+    var speedBase = 0f
+    var maxSpeed = 5f
+    var alpha = 0f
+    var accInterpolation: Interpolation = Interpolation.exp10Out
+    var cosDeg = 0f
+    var sinDeg = 0f
+    var root = false
+
     companion object {
         val mapper = mapperFor<MoveComponent>()
     }
