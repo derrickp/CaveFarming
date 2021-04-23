696c6d9cb3862842a0d50fa74925c905279883ee
Derrick Plotsky <derrickplotsky@gmail.com>
2021-04-22 18:53:49 -0700

    Add dependencies for box2d
    And one for Tiled.

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,12 +16,15 @@ java {
 dependencies {
     api("com.badlogicgames.ashley:ashley:$ashleyVersion")
     api("com.badlogicgames.gdx:gdx:$gdxVersion")
+    api("com.badlogicgames.gdx:gdx-box2d:$gdxVersion")
     api("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     api("io.github.libktx:ktx-app:$ktxVersion")
     api("io.github.libktx:ktx-ashley:$ktxVersion")
     api("io.github.libktx:ktx-assets:$ktxVersion")
+    api("io.github.libktx:ktx-box2d:$ktxVersion")
     api("io.github.libktx:ktx-collections:$ktxVersion")
     api("io.github.libktx:ktx-graphics:$ktxVersion")
     api("io.github.libktx:ktx-inject:$ktxVersion")
     api("io.github.libktx:ktx-log:$ktxVersion")
+    api("io.github.libktx:ktx-tiled:$ktxVersion")
 }
