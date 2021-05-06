71f48597706844dc7254723d53ec3e22179b39f6
Derrick Plotsky <derrickplotsky@gmail.com>
2021-05-05 20:13:35 -0700

    Add crops as specific configured Items
    These will be used inside of our Inventory component as a way of keeping
    track of the number of crops that have been harvested.

--- core/src/main/kotlin/dev/plotsky/cavefarming/inventory/Items.kt
+++ core/src/main/kotlin/dev/plotsky/cavefarming/inventory/Items.kt
@@ -1,5 +1,7 @@
 package dev.plotsky.cavefarming.inventory
 
+import dev.plotsky.cavefarming.crops.CropType
+
 object Items {
     private const val LIMIT_ONE = 1
 
@@ -17,5 +19,46 @@ object Items {
             LIMIT_ONE,
             ItemType.WEARABLE
         )
+    }
+
+    private val harvestedMushrooms by lazy {
+        Item(
+            name = CropType.MUSHROOMS.name,
+            Int.MAX_VALUE,
+            ItemType.HARVESTED_ITEM
+        )
+    }
+
+    private val harvestedKanes by lazy {
+        Item(
+            name = CropType.KANES.name,
+            Int.MAX_VALUE,
+            ItemType.HARVESTED_ITEM
+        )
+    }
+
+    private val harvestedPotatoes by lazy {
+        Item(
+            name = CropType.POTATOES.name,
+            Int.MAX_VALUE,
+            ItemType.HARVESTED_ITEM
+        )
+    }
+
+    private val harvestedTurnips by lazy {
+        Item(
+            name = CropType.TURNIPS.name,
+            Int.MAX_VALUE,
+            ItemType.HARVESTED_ITEM
+        )
+    }
+
+    fun itemByCropType(cropType: CropType): Item {
+        return when(cropType) {
+            CropType.MUSHROOMS -> harvestedMushrooms
+            CropType.KANES -> harvestedKanes
+            CropType.POTATOES -> harvestedPotatoes
+            CropType.TURNIPS -> harvestedTurnips
+        }
     }
 }
